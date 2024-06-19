2fe7dee1a09d7f0d40e9022c04de5b59845f887c
João Victor da Silva <jv782063@gmail.com>
2024-06-19 15:10:19 -0300

    reset

--- cmd/game_server/main.go
+++ cmd/game_server/main.go
@@ -1,22 +1,4 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/joaovds/stop-go/internal/application/usecases/roomcasesimpl"
-	"github.com/joaovds/stop-go/internal/infra/database/memory/memrepos"
-)
-
 func main() {
-	room_repo := memrepos.NewRoomRepository()
-	list_room_uc := roomcasesimpl.NewListRoomUseCaseImpl(room_repo)
-
-	rooms, err := list_room_uc.Execute()
-	if err != nil {
-		panic(err)
-	}
-
-	for _, room := range rooms {
-		fmt.Printf("Room: %v\n", room)
-	}
 }
